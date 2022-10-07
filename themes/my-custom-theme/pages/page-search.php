343ca9a1ea622d95a18729430b91531df556cf19
Liudmila Kazakbaeva <83576552+liudmilitas@users.noreply.github.com>
2022-10-07 16:26:53 +0200

    added a search page

---
+++ themes/my-custom-theme/pages/page-search.php
@@ -0,0 +1,17 @@
+<?php /* Template Name: Search Template */?>
+
+<?php get_header(); ?>
+
+
+<header class="header">
+</header>
+
+<div class="search-wrapper">
+    <h2 class="page-title">
+        <?php the_title(); ?>
+    </h2>
+    <h4>What are you looking for?</h4>
+    <?php get_search_form(); ?>
+</div>
+
+<?php get_footer(); ?>
